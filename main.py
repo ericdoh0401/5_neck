aa0aaa805e19773b40aae5a01ab3df3aa57c472d
ericdoh0401 <123275243+ericdoh0401@users.noreply.github.com>
2025-08-05 18:29:22 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from base.py import Game
+from base import *
+
+if __name__ == "__main__":
+
+   boardDim = int(input("Please input board dimension: "))
+
+   game = Game(boardDim)
+
+   game.displayBoard()
+
+   
