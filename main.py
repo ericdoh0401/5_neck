2856018122ca9da78962a5a41170baaa91bade88
ericdoh0401 <123275243+ericdoh0401@users.noreply.github.com>
2025-08-06 20:18:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ if __name__ == "__main__":
 
    boardDim = int(input("Please input board dimension: "))
 
-   game = Game(boardDim)
+   game, curPlayer = Game(boardDim), 0
 
-   game.displayBoard()
+   while game.gameStatus:
+      coord = list(input("Where would you like to place your next stone?: "))
+      
+      while not game.makeMove(curPlayer, coord):
+         coord = list(input("Previous input was invalid. Please select a new coordinate: "))
 
-   
+      if game.gameStatus:
+         curPlayer = (curPlayer + 1) % 2
+
+   print(f"Congratulations! Player {curPlayer} has won the game!")
