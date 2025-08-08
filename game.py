20641ae77315fa27b2835bce8071693e0440c61c
ericdoh0401 <123275243+ericdoh0401@users.noreply.github.com>
2025-08-08 20:27:57 +0900

    Update game.py

--- game.py
+++ game.py
@@ -10,16 +10,34 @@ class Game:
     self.player2 = Player2()
 
   def makeMove(self, player, coord):
+    # Paramters:
+    # player (int) -> the player who is to place the stone
+    # coord (tuple) -> the coordinate of the stone the player wishes to place
+
+    # Return:
+    # bool -> True if the stone the player places is a valid placement, thus moving onto the next player. False otherwise
 
     if player == 0:
+      valid = self.player1.isValidPlacement(coord)
+      if not valid:
+        print("This is not a valid placement for your stone. Either it creates a connect-6 or greater or you have created a 삼삼.")
+        return False
+
+      self.gameBoard.placeStone(player, coord)
+      currentStatus = self.player1.makeFive()
+
+      if currentStatus:
+        self.gameStatus = False
+
+      return True
       # black player movement
 
     else:
-      currrentStatus = self.player2.checkAndSee(coord)
       self.gameBoard.placeStone(player, coord)
+      currrentStatus = self.player2.checkAndSee(coord)
 
       if currentStatus:
-        # this means that player 2 has won
+        # this means that player 2 has won and the game has ended
         self.gameStatus = False
 
       return True
