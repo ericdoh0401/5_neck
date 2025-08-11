01d2c8e4663223f97ec3d3aae1d41b719646e2a4
ericdoh0401 <123275243+ericdoh0401@users.noreply.github.com>
2025-08-11 20:12:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import game
+from collections import defaultdict
 
 if __name__ == "__main__":
 
    boardDim = int(input("Please input board dimension: "))
 
-   game, curPlayer = Game(boardDim), 0
+   game, curPlayer = game.Game(boardDim), 0
 
    while game.gameStatus:
-      coord = list(input("Where would you like to place your next stone?: "))
+      coord = (input("Where would you like to place your next stone?: ")).split()
+
+      coord = tuple([int(i) for i in coord])
       
       while not game.makeMove(curPlayer, coord):
-         coord = list(input("Previous input was invalid. Please select a new coordinate: "))
+         coord = (input("Previous placement was invalid. Choose a new position: ")).split()
+         coord = tuple([int(i) for i in coord])
 
       if game.gameStatus:
          curPlayer = (curPlayer + 1) % 2
 
-   print(f"Congratulations! Player {curPlayer} has won the game!")
+      game.gameBoard.displayBoard()
+
+   print(f"Congratulations! Player {curPlayer + 1} has won the game!")
