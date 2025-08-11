9975fcc2812ff48b3404d283cd4c9bc0d208a04a
ericdoh0401 <123275243+ericdoh0401@users.noreply.github.com>
2025-08-11 20:12:20 +0900

    Update player2.py

--- player2.py
+++ player2.py
@@ -1,13 +1,18 @@
+from collections import defaultdict
+import board
+
 class Player2:
   def __init__(self):
     self.placements = set()
 
-  def checkAndSee(self, coord):
+  def checkAndSee(self, coord, board):
     # Parameters:
     # coord (tuple) -> the coordinate of the stone that player 2 has placed during his most recent turn
 
     # Return:
     # bool -> True if the stone he placed allows player 2 to win, else False
+
+    x, y = coord
     
     self.placements.add(coord)
     
@@ -17,46 +22,46 @@ class Player2:
       tmpX, tmpY = x, y
 
       if i == 0:
-        while tmpX >= 0 and tmpY >= 0 and self.board[tmpX][tmpY] == 'x':
+        while tmpX >= 0 and tmpY >= 0 and board.board[tmpX][tmpY] == 'x':
           surroundings[i] += 1
           tmpX -= 1
           tmpY -= 1
     
       elif i == 1:
-        while tmpX >= 0 and self.board[tmpX][tmpY] == 'x':
+        while tmpX >= 0 and board.board[tmpX][tmpY] == 'x':
           surroundings[i] += 1
           tmpX -= 1
     
       elif i == 2:
-        while tmpX >= 0 and tmpY < len(board[0]) and self.board[tmpX][tmpY] == 'x':
+        while tmpX >= 0 and tmpY < len(board.board[0]) and board.board[tmpX][tmpY] == 'x':
           surroundings[i] += 1
           tmpX -= 1
           tmpY += 1
     
       elif i == 3:
-        while tmpY < len(board[0]) and self.board[tmpX][tmpY] == 'x':
+        while tmpY < len(board.board[0]) and board.board[tmpX][tmpY] == 'x':
           surroundings[i] += 1
           tmpY += 1
     
       elif i == 4:
-        while tmpX < len(board) and tmpY < len(board[0]) and self.board[tmpX][tmpY] == 'x':
+        while tmpX < len(board.board) and tmpY < len(board.board[0]) and board.board[tmpX][tmpY] == 'x':
           surroundings[i] += 1
           tmpX += 1
           tmpY += 1
     
       elif i == 5:
-        while tmpX < len(board) and self.board[tmpX][tmpY] == 'x':
+        while tmpX < len(board.board) and board.board[tmpX][tmpY] == 'x':
           surroundings[i] += 1
           tmpX += 1
     
       elif i == 6:
-        while tmpX < len(board) and tmpY >= 0 and self.board[tmpX][tmpY] == 'x':
+        while tmpX < len(board.board) and tmpY >= 0 and board.board[tmpX][tmpY] == 'x':
           surroundings[i] += 1
           tmpX += 1
           tmpY -= 1
     
       elif i == 7:
-        while tmpY >= 0 and self.board[tmpX][tmpY] == 'x':
+        while tmpY >= 0 and board.board[tmpX][tmpY] == 'x':
           surroundings[i] += 1
           tmpY -= 1
 
